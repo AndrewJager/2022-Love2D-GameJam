a8a8c74c4cbb0eeb65a82fa3aea35cb44cb9dc74
CatsAreEvil <35353143+CatsAreEvil@users.noreply.github.com>
2022-02-25 18:25:23 -0500

    Setup ability to script events by time

--- main.lua
+++ main.lua
@@ -1,11 +1,30 @@
+local scene = require("scenescripter")
+local test = "0"
+
 function love.load()
-   
+    scene.load()
+
+    local function a()
+        test = "1"
+    end
+    local function b()
+        test = "2"
+    end
+    local function c()
+        test = "3"
+    end
+   scene.addEvent(a, 1)
+   scene.addEvent(b, 2)
+   scene.addEvent(c, 4)
+
+   scene.start()
 end
 
 function love.update(dt)
-
+    scene.update(dt)
 end
 
 function love.draw()
-    love.graphics.print("We're doomed", 10, 10)
+    love.graphics.print(test, 10, 10)
+    scene.draw()
 end
---
+++ scenescripter.lua
@@ -0,0 +1,46 @@
+local a = {}
+
+local function load()
+    a.events = {}
+    a.started = false
+    a.timepassed = 0
+    a.starttime = 0
+end
+a.load = load
+
+local function update(dt)
+    if a.started then
+        a.timepassed = love.timer.getTime() - a.starttime
+
+        for i = 1, #a.events do
+            local event = a.events[i]
+            if not event.fired then
+                if a.timepassed >= event.time then
+                    event.fired = true
+                    event.func()
+                end
+            end
+        end
+    end
+end
+a.update = update
+
+local function draw()
+    -- love.graphics.print(a.timepassed .. "   " .. test, 10, 200)
+end
+a.draw = draw
+
+local function addEvent(eventFunc, time)
+    local event = {func=eventFunc, time=time, fired=false}
+    table.insert(a.events, event)
+end
+a.addEvent = addEvent
+
+local function start()
+    a.starttime = love.timer.getTime()
+    a.started = true
+end
+a.start = start
+
+
+return a
